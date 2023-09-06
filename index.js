a22e2ae06f62431fc1174564c9f3570ee6d606d6
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2023-09-06 09:15:42 +0200

    chore: Refactor program structure and file organization
    - Rename `lib/fixtures.js` to `lib/cfs/fixtures.js`
    - Change the program name from "vz-sched" to "aos-sched"
    - Update the description of the program
    - Add an argument for the scheduler to use, with a validation for "cfs"
    - Remove the "num" option
    - Change the parse function to use the "program.run" method
    - Update the dependency `caporal` to `@caporal/core` version `2.0.7`
    - Add a `bin` configuration for the executable `aos-sched`
    - File `lib/__snapshots__/lib.test.js.snap` was renamed to `lib/cfs/__snapshots__/lib.test.js.snap`
    - Add `.gitignore` file with `package-lock.json`
    - Rename `lib/lib.test.js` to `lib/cfs/lib.test.js`
    - Rename `lib/lib.js` to `lib/cfs/lib.js`
    - Change the import path for `./artifacts` to `../artifacts`

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 #!/usr/bin/env node
 "use strict";
 
-const name = "vz-sched";
-const prog = require("caporal");
-const { runAndSave } = require("./lib");
+const { program } = require("@caporal/core");
+const { runAndSave } = require("./lib/cfs/lib");
 // const { schedule } = require("./lib/" + name + "/fixtures");
 let $fs = require("mz/fs");
 let $gstd = require("get-stdin");
 
 let main = () => {
-  prog
-    .description("Swiss Knife tool schedule diagrams")
+  program
+    .name("aos-sched")
+    .description("Create temporal diagrams of AOS realtime schedulers")
+    .argument("<sched>", "Scheduler to use", {
+      // just provide an array
+      validator: ["cfs"],
+    })
     .argument("[json]", "JSON file or stdin")
     .option(
       "-s, --save <string>",
       "save data with in files with prefix <string>"
     )
     .option("-w, --draw", "produce only latex code for drawing")
-    .option("-n, --num <int>", "which test schedule", prog.INT, 9)
     .action((args, options) => {
       let datap = args.json ? $fs.readFile(args.json, "utf8") : $gstd();
       datap.then(JSON.parse).then((sched) => {
         runAndSave(options, sched);
       });
     });
-  prog.parse(process.argv);
+  program.run(process.argv);
 };
 
 main();
